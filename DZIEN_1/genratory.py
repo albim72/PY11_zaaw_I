c4f9a5e83f419895d3b366883cb4ce048f032569
albim72 <marcin.albiniak72@gmail.com>
2024-11-04 12:17:49 +0100

    Update genratory.py

--- DZIEN_1/genratory.py
+++ DZIEN_1/genratory.py
@@ -17,3 +17,34 @@ print(list(genliczby()))
 
 for p in genliczby():
     print(p)
+
+#przykład 2
+def wznowienie(n,k):
+    print("wstrzymanie działania 0")
+    yield 3005
+    print("wznowienie działanie 1")
+
+    print("wstrzymanie działania 1")
+    yield n+k
+    print("wznowienie działanie 2")
+
+    print("wstrzymanie działania 2")
+    n = 8*k - 4
+    yield n-k
+    print("wznowienie działanie 3")
+
+    print("wstrzymanie działania 3")
+    yield n*k
+    print("wznowienie działanie 4")
+
+    print("wstrzymanie działania 4")
+    yield n/k
+    print("wznowienie działanie 5")
+
+print(wznowienie(8,4))
+print(list(wznowienie(8,4)))
+
+for i in wznowienie(8,4):
+    print("_"*50)
+    print(type(i))
+    print(f'zwrócono wartośc: {i}')
