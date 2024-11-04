9640cee0141fd69c8e77d7e8ab775d2536fccffd
albim72 <marcin.albiniak72@gmail.com>
2024-11-04 11:02:06 +0100

    Update funkcja_w_funkcji.py

--- DZIEN_1/funkcja_w_funkcji.py
+++ DZIEN_1/funkcja_w_funkcji.py
@@ -17,3 +17,34 @@ def osoba(funkcja:Any,*args)-> Any:
 print(osoba(witaj,"Leon"))
 print(osoba(konkurs,"Anna",78,9))
 print(osoba(bonus,66,0.2))
+
+#przykład 2 - prosty dekorator
+
+def startstop(funkcja):
+    def wrapper(*args):
+        print("_"*60)
+        print("startowanie procesu...")
+        funkcja(*args)
+        print("kończenie procesu...")
+    return wrapper
+
+def zawijanie(w_co):
+    print(f"zawijanie czekoladek w {w_co}")
+
+zw = startstop(zawijanie)
+print(zw)
+zw("sreberka")
+
+@startstop
+def dmuchanie(czego):
+    print(f"dmuchanie {czego} na urodziny!")
+
+@startstop
+def info():
+    print("@ - dekoracja bezwzględna!")
+
+dmuchanie("baloników")
+dmuchanie("świeczek na torcie")
+info()
+
+#przykład 3
