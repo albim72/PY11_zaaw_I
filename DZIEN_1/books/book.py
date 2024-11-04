101269cccd5661e71089dd8579340ea3446586da
albim72 <marcin.albiniak72@gmail.com>
2024-11-04 15:30:53 +0100

    Create book.py

---
+++ DZIEN_1/books/book.py
@@ -0,0 +1,22 @@
+from abc import ABC,abstractmethod
+
+class Book(ABC):
+    def __init__(self,title,author,year,pages):
+        self.title = title
+        self.author = author
+        self.year = year
+        self.pages = pages
+
+    def __repr__(self):
+        return f"obiekt klasy {self.__class__.__name__} ({self.title} -> {self.author})"
+
+    def __call__(self, price):
+        return f"ilośc stron z dodatkami: {self.pages + 5}, cena: {price} zł"
+
+    @abstractmethod
+    def get_info(self):
+        pass
+
+    @abstractmethod
+    def read(self):
+        pass
