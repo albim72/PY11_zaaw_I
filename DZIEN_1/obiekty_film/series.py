bd5cdaeca4b7e631d9601ab979abdbfd6702c9f7
albim72 <marcin.albiniak72@gmail.com>
2024-11-04 14:46:26 +0100

    Create series.py

---
+++ DZIEN_1/obiekty_film/series.py
@@ -0,0 +1,7 @@
+class SeriesPart:
+    def __init__(self,series_name:str,part_number:int):
+        self.series_name = series_name
+        self.part_number = part_number
+
+    def get_series_info(self):
+        return f"Seria: {self.series_name}, częśc: {self.part_number}."
