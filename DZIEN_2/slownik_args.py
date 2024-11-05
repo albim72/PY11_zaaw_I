3f4cb51a641b2afff1d9706c64153bfa23a92d7b
albim72 <marcin.albiniak72@gmail.com>
2024-11-05 11:35:05 +0100

    Update slownik_args.py

--- DZIEN_2/slownik_args.py
+++ DZIEN_2/slownik_args.py
@@ -20,3 +20,19 @@ def create_user_profile(**kwargs):
 user_profile = create_user_profile(username = "Jan Nowak",email="janek@abc.pl",
                                        age=45,city="Kraków")
 print(user_profile)
+
+
+#przykład 3 - przekazywanie argumentów do innych funkcji
+def print_info(name,age,**kwargs):
+    print(f"name: {name}, age: {age}")
+    if kwargs:
+        print("dodana informacja ->")
+        for key,value in kwargs.items():
+            print(f"{key}: {value}")
+
+def user_details(**kwargs):
+    print_info(**kwargs)
+
+user_details(name="Alicja",age=27,location="Sopot",profession="inżynier sieci")
+user_details(name="Leon",age=62,location="Opole")
+user_details(name="Maciej",age=16,location="Lublin",eyes="niebieskie")
