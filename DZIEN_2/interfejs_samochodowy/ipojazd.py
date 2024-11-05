069b35bcefce5eb38e6b47fa042a5023f42c41bf
albim72 <marcin.albiniak72@gmail.com>
2024-11-05 10:33:35 +0100

    Update ipojazd.py

--- DZIEN_2/interfejs_samochodowy/ipojazd.py
+++ DZIEN_2/interfejs_samochodowy/ipojazd.py
@@ -3,6 +3,9 @@ from abc import ABCMeta, abstractmethod
 class IPojazd(metaclass=ABCMeta):
     @abstractmethod
     def spalanie(self,odl,jedn):raise NotImplementedError
-    
+
     @abstractmethod
     def kosztyprzejazdu(self,odl,jedn,cena):raise NotImplementedError
+
+    @abstractmethod
+    def info(self,i):raise NotImplementedError
