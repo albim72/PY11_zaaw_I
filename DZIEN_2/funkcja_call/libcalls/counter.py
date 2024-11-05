754d44801ffa5c62aa6a95da45063a16e239bc5f
albim72 <marcin.albiniak72@gmail.com>
2024-11-05 11:01:49 +0100

    Create counter.py

---
+++ DZIEN_2/funkcja_call/libcalls/counter.py
@@ -0,0 +1,7 @@
+class CallCounter:
+    def __init__(self):
+        self.count = 0
+        
+    def __call__(self, *args, **kwargs):
+        self.count += 1
+        print(f"funkcję call wywołano {self.count} razy")
