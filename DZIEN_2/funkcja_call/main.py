fa7479bfdb295486951280a4841e8b96be88607b
albim72 <marcin.albiniak72@gmail.com>
2024-11-05 11:11:02 +0100

    Update main.py

--- DZIEN_2/funkcja_call/main.py
+++ DZIEN_2/funkcja_call/main.py
@@ -5,9 +5,18 @@ Trzy przypadki użycia funckji __call__
 3. implementacja z pamięcią podręczną  (caching)
 """
 from libcalls.counter import CallCounter
+from libcalls.validator import RangeValidator
 
+print("*"*70)
 counter = CallCounter()
 counter()
 counter()
 counter()
 counter()
+
+print("*"*70)
+valid = RangeValidator(2,28)
+valid(3)
+valid(17)
+valid(0)
+valid(44)
