75b0d73f4a28eda24d84ac656eecda52cf839614
albim72 <marcin.albiniak72@gmail.com>
2024-11-06 09:51:13 +0100

    Update metaklasa.py

--- DZIEN_3/metaklasa.py
+++ DZIEN_3/metaklasa.py
@@ -5,7 +5,35 @@ class MojaMeta(type):
         print(f"klasy dziedziczone: {superclasses}")
         print(f"słownik atrybutów klasy: {attrs}")
         return type.__new__(cls,clsname,superclasses,attrs)
-    
+
     def jedynka(cls):
         return 1
-    
+
+class S:
+    pass
+
+class F:
+    pass
+
+class Specjalna(S,metaclass=MojaMeta):
+    pass
+
+class B(Specjalna):
+    pass
+
+class C(F,B):
+    @property
+    def info(self):
+        print("abc...")
+
+class D(F):
+    pass
+
+obiekt_c = C()
+# print(obiekt_c.jedynka())
+
+klasa_c = C
+print(type(obiekt_c))
+print(type(klasa_c))
+
+print(klasa_c.jedynka())
