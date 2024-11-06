f1f423c5c9a29833dbbd78b1aecc44f86326287e
albim72 <marcin.albiniak72@gmail.com>
2024-11-06 11:02:19 +0100

    Update abstract_factory.py

--- WZORCE_PROJEKTOWE/abstract_factory.py
+++ WZORCE_PROJEKTOWE/abstract_factory.py
@@ -3,19 +3,29 @@ from abc import ABC, abstractmethod
 #metaklasa do śledzenia rejestracji fabryk
 class FactoryMeta(type):
     factories = {}
-    
+
     def __new__(cls, name, bases, attrs):
         new_cls = super().__new__(cls, name, bases, attrs)
         if not name.startswith('Abstract'):
             FactoryMeta.factories[name] = new_cls
         return new_cls
-    
+
 #klasa abstrakcyjna dla dokumentów
 class AbstractDocument(ABC):
     @abstractmethod
     def generate_content(self):
         pass
-    
+
     def display(self):
         print("Displaying Content")
         print(self.generate_content())
+        
+        
+#konkretna implementacja dokumentów
+class InvoiceDocument(AbstractDocument):
+    def generate_content(self):
+        return "Treśc faktury: Detale trasakcji, kwoty, daty."
+    
+class ReportDocument(AbstractDocument):
+    def generate_content(self):
+        return "Treśc raportu: Analizy danych, podsumowanie miesięczne."
