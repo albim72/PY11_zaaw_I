4f223133e753c907295978c2fc6bb003bf4e5f73
albim72 <marcin.albiniak72@gmail.com>
2024-11-06 15:26:29 +0100

    Create kompozycja.py

---
+++ DZIEN_3/kompozycja.py
@@ -0,0 +1,25 @@
+class KontrolerSamochodu(type):
+    def __new__(cls,name,bases,attrs):
+        print(f"Tworzenie klasy samochodu: {name}")
+        attrs['informacje'] = lambda self:f"Model: {self.model}, moc silnika: {self.silnik.moc}"
+        return super().__new__(cls,name,bases,attrs)
+
+class Silnik:
+    def __init__(self,moc):
+        self.moc = moc
+
+    def uruchom(self):
+        return f"Silnik o mocy {self.moc} KM został uruchomiony!"
+
+class Samochod(metaclass=KontrolerSamochodu):
+    def __init__(self,model,moc_silnika):
+        self.model = model
+        self.silnik = Silnik(moc_silnika)
+
+    def uruchom_samochod(self):
+        return f"Samochód {self.model}: {self.silnik.uruchom()}"
+
+silnik1 = Silnik(150)
+samochod1 = Samochod("Tesla Model S",670)
+print(samochod1.uruchom_samochod())
+print(samochod1.informacje())
