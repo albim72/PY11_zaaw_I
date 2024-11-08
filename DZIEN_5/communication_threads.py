c122b45eb8576fb2935e382603bca7dc562366ff
albim72 <marcin.albiniak72@gmail.com>
2024-11-08 10:21:25 +0100

    Create communication_threads.py

---
+++ DZIEN_5/communication_threads.py
@@ -0,0 +1,34 @@
+import threading
+import queue
+import time
+
+
+data_queue = queue.Queue()
+
+def producer():
+    for i in range(5):
+        item = f'item {i}'
+        print(f'Poducent: produkuję {item}')
+        data_queue.put(item)
+        time.sleep(1)
+
+def consumer():
+    while True:
+        item = data_queue.get()
+        if item is None:
+            break
+        print(f'Konsument: odbieram {item}')
+        data_queue.task_done()
+
+producer_thread = threading.Thread(target=producer)
+consumer_thread = threading.Thread(target=consumer)
+
+producer_thread.start()
+consumer_thread.start()
+
+producer_thread.join()
+
+data_queue.put(None)
+consumer_thread.join()
+
+print("koniec komunikacji między wątkami")
