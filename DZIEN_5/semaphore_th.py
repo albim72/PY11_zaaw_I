a605134f590dec7112a936945b79810f0e5aaf27
albim72 <marcin.albiniak72@gmail.com>
2024-11-08 11:13:11 +0100

    Create semaphore_th.py

---
+++ DZIEN_5/semaphore_th.py
@@ -0,0 +1,29 @@
+import threading
+import time
+import random
+
+
+semaphore = threading.Semaphore(3)
+
+def access_resource(thread_id):
+    print(f"Wątek {thread_id} czeka na dotęp do zasobu...")
+    semaphore.acquire()
+    try:
+        print(f"Wątek {thread_id} uzyskał dostęp do zasobu...")
+        time_to_sleep = random.uniform(1,3)
+        time.sleep(time_to_sleep)
+        print(f"Wątek {thread_id} zwalnia zasób po {time_to_sleep} sekundach")
+    finally:
+        semaphore.release()
+
+threads = []
+
+for i in range(10):
+    t = threading.Thread(target=access_resource,args=(i,))
+    threads.append(t)
+    t.start()
+
+for t in threads:
+    t.join()
+
+print("Wszytkie wątki zostały zakończone!")
